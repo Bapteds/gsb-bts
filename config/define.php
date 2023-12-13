2df0d502a51382d627190d778a14621d4dbf71dc
Bapteds <eradesbapt@gmail.com>
2023-12-13 15:14:15 +0100

    Initial commit

---
+++ config/define.php
@@ -0,0 +1,5 @@
+<?php
+
+define('PATH_VIEWS', '../src/Vues/');
+define('PATH_VIEWS_COMPTABLE', '../src/Vues/comptable/');
+define('PATH_CTRLS', '../src/Controleurs/');
---
+++ src/Vues/comptable/v_accueil_comptable.php
@@ -0,0 +1,42 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+?>
+
+<div class="alert alert-warning" role="alert"><strong>Rappel : </strong>Lorem Ipsum
+</div>
+<div id="accueil">
+    <h2>
+        Éspace comptable<small> - <?= $_SESSION['role'] ?> : 
+            <?= $_SESSION['prenom'] . ' ' . $_SESSION['nom'] ?></small>
+    </h2>
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    <span class="glyphicon glyphicon-bookmark"></span>
+                    Gestions
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-xs-12 col-md-12">
+                        <a href="index.php?uc=gererFrais&action=saisirFrais"
+                           class="btn btn-success btn-lg" role="button">
+                            <span class="glyphicon glyphicon-pencil"></span>
+                            <br>Renseigner la fiche de frais</a>
+                        <a href="index.php?uc=etatFrais&action=selectionnerMois"
+                           class="btn btn-primary btn-lg" role="button">
+                            <span class="glyphicon glyphicon-list-alt"></span>
+                            <br>Afficher mes fiches de frais</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
