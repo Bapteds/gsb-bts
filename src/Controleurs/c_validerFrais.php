4d667cf367f82c49390272d45514b7e45ddaebfd
Bapteds <eradesbapt@gmail.com>
2024-03-20 08:26:05 +0100

    1

--- src/Controleurs/c_validerFrais.php
+++ src/Controleurs/c_validerFrais.php
@@ -3,31 +3,39 @@
 use Outils\Utilitaires;
 
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$visiteurs = $pdo->getTousLesVisteurCloture();
+$dates = $pdo->getTousLesMois();
 switch ($action) {
     case 'choisirClientDate':
-        $visiteurs = $pdo->getTousLesVisteurCloture();
-        $dates = $pdo->getTousLesMois();
         require PATH_VIEWS_COMPTABLE . 'v_choixClientDate.php';
         break;
     case 'voirClient':
-        $idVisiteur = filter_input(INPUT_POST, 'visiteur', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $mois = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $_SESSION['idVisiteur'] = filter_input(INPUT_POST, 'visiteur', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $_SESSION['mois'] = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         // Ici grave à mon $post je récupère les éléments associés à mon visiteur en tant que comptable
-        $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $mois);
-        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $mois);
-        require PATH_VIEWS_COMPTABLE . 'v_afficheFraisForfait.php';
-        require PATH_VIEWS_COMPTABLE . 'v_afficheFraisHorsForfait.php';
-        break;
+        $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($_SESSION['idVisiteur'], $_SESSION['mois']);
+        $lesFraisForfait = $pdo->getLesFraisForfait($_SESSION['idVisiteur'], $_SESSION['mois']);
+        if ($lesFraisForfait == null && $lesFraisHorsForfait == null) {
+            Utilitaires::ajouterErreur('Pas de fiche de frais pour ce visiteur ce mois');
+            include PATH_VIEWS . 'v_erreurs.php';
+        } else {
+            require PATH_VIEWS_COMPTABLE . 'v_choixClientDate.php';
+
+            require PATH_VIEWS_COMPTABLE . 'v_afficheFraisForfait.php';
+            require PATH_VIEWS_COMPTABLE . 'v_afficheFraisHorsForfait.php';
+        }break;
     case 'validerMajCorrection':
-        $idVisiteur = filter_input(INPUT_POST, 'visiteur', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $mois = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $lesFrais = filter_input(INPUT_POST, 'lesFrais', FILTER_DEFAULT, FILTER_FORCE_ARRAY);
         if (Utilitaires::lesQteFraisValides($lesFrais)) {
-            $pdo->majFraisForfait($idVisiteur, $mois, $lesFrais);
+            $pdo->majFraisForfait($_SESSION['idVisiteur'], $_SESSION['mois'],$lesFrais);
         } else {
             Utilitaires::ajouterErreur('Les valeurs des frais doivent être numériques');
             include PATH_VIEWS . 'v_erreurs.php';
         }
+        break;
+    case 'devTest':
+        dd($_POST);
+        
         break;
 }
 
--- src/Vues/comptable/v_afficheFraisHorsForfait.php
+++ src/Vues/comptable/v_afficheFraisHorsForfait.php
@@ -1,43 +1,48 @@
 <?php
-
-/* 
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
-
 ?>
 <hr>
 <div class="row">
     <div class="panel panel-info">
         <div class="panel-heading">Descriptif des éléments hors forfait</div>
-        <table class="table table-bordered table-responsive">
-            <thead>
-                <tr>
-                    <th class="date">Date</th>
-                    <th class="libelle">Libellé</th>  
-                    <th class="montant">Montant</th>  
-                    <th class="action">&nbsp;</th> 
-                </tr>
-            </thead>  
-            <tbody>
-            <?php
-            foreach ($lesFraisHorsForfait as $unFraisHorsForfait) {
-                $libelle = htmlspecialchars($unFraisHorsForfait['libelle']);
-                $date = $unFraisHorsForfait['date'];
-                $montant = $unFraisHorsForfait['montant'];
-                $id = $unFraisHorsForfait['id']; ?>           
-                <tr>
-                    <td> <?php echo $date ?></td>
-                    <td> <?php echo $libelle ?></td>
-                    <td><?php echo $montant ?></td>
-                    <td>
-                        Corriger/Reinitialiser
-                    </td>
-                </tr>
-                <?php
-            }
-            ?>
-            </tbody>  
-        </table>
+        <form method="POST" action="index.php?uc=validerFrais&action=devTest" >
+            <table class="table table-bordered table-responsive">
+                <thead>
+                    <tr>
+                        <th class="date">Date</th>
+                        <th class="libelle">Libellé</th>  
+                        <th class="montant">Montant</th>  
+                        <th class="action">&nbsp;</th> 
+                    </tr>
+                </thead>  
+                <tbody>
+                    <?php
+                    foreach ($lesFraisHorsForfait as $unFraisHorsForfait) {
+                        $date = $unFraisHorsForfait[4];
+                        $montant = $unFraisHorsForfait['montant'];
+                        $id = $unFraisHorsForfait['id'];
+                        $libelle = htmlspecialchars($unFraisHorsForfait['libelle']);
+                        ?>       
+                        <tr>
+                            <td><input type="date" value="<?php echo $date ?>" name="date/<?php echo $id ?>"></td>
+                            <td><input type="text" value="<?php echo $libelle ?>" name="libelle/<?php echo $id ?>"></td>
+                            <td><input type="number" value="<?php echo $montant; ?>" name="montant/<?php echo $id ?>"</td>
+                            <td>
+                                <input type="submit" class="btn btn-success" value="Corriger">
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                </tbody>  
+            </table>
+        </form>
+    </div>
+    <div>
+        <button class="btn btn-success" type="submit">Valider</button>
+        <button class="btn btn-danger" type="reset">Reinitialiser</button>
     </div>
 </div>
